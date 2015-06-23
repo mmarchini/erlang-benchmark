f97848ac127bd004b72e881b648aa4993a78aee1
Matheus Marchini <matheusdot@gmail.com>
2015-06-23 17:04:27 -0300

    Matrix multiplication parameters

--- src/matrixMultiplication.erl
+++ src/matrixMultiplication.erl
@@ -1,5 +1,5 @@
 -module(matrixMultiplication).
--export([rotateMatrix/1, matrixMultiplication/2, vectorMultiplication/2, multi1/2, multi2/2]).
+-export([matrixMultiplication/2, test_loop/1, test_loop/4]).
 
 matrixMultiplication(A, B) ->
     multi1(A, rotateMatrix(B)).
@@ -30,3 +30,51 @@ vectorMultiplication([], []) ->
 vectorMultiplication([A | ATail], [B | BTail]) ->
     (A * B) + vectorMultiplication(ATail, BTail).
     
+%%%%%%%%%%%%%%%%%%%
+%% PROFILE STUFF %%
+%%%%%%%%%%%%%%%%%%%
+
+profile(A, B) ->
+    timer:tc(matrixMultiplication, matrixMultiplication, [A, B]).
+
+test_matrixMultiplication(A, B, PerfFile) ->
+    {Time, Result} = profile(A, B),
+    file:write(PerfFile, io_lib:fwrite("~w\n", [Time])), Result.
+
+test_looper(_, _, 0, _) ->
+    ok;
+test_looper(A, B, Index, PerfFile) ->
+    test_matrixMultiplication(A, B, PerfFile),
+    test_looper(A, B, Index-1, PerfFile).
+
+granted_int(Num) when erlang:is_integer(Num) -> 
+    Num;
+granted_int(Num) when erlang:is_list(Num) -> 
+    {NewNum, _} = string:to_integer(Num),
+    NewNum;
+granted_int(Num) when erlang:is_atom(Num) -> 
+    granted_int(erlang:atom_to_list(Num)).
+
+problemFromFileAux(_, eof) ->
+    [];
+problemFromFileAux(Device, Line) ->
+    [[granted_int(Num) || Num <- string:tokens(Line, " ")]] ++ problemFromFileAux(Device, io:get_line(Device, "")).
+
+problemFromFileAux(Device) ->
+    problemFromFileAux(Device, io:get_line(Device, "")).
+
+matrixFromFile(Filename) ->
+    {ok, File} = file:open(Filename, [read]),
+    Matrix = problemFromFileAux(File),
+    file:close(File),
+    Matrix.
+
+test_loop(AFile, BFile, Repetitions, PerfFile) ->
+    {ok, PFile} = file:open(PerfFile, [write]),
+    Result = test_looper(matrixFromFile(AFile), matrixFromFile(BFile), granted_int(Repetitions), PFile),
+    file:close(PFile),
+    Result.
+
+test_loop([AFile, BFile, Repetitions, PerfFile]) ->
+    test_loop(AFile, BFile, Repetitions, PerfFile).
+
--- src/parallelMatrixMultiplication.erl
+++ src/parallelMatrixMultiplication.erl
@@ -1,5 +1,5 @@
 -module(parallelMatrixMultiplication).
--export([matrixMultiplication/3, matrixMultiplicationSpawn/3, scatter/2]).
+-export([matrixMultiplication/3, matrixMultiplicationSpawn/3, test_loop/1, test_loop/5]).
 
 gather([H|T]) ->
     receive
@@ -54,3 +54,52 @@ vectorMultiplication([], []) ->
 vectorMultiplication([A | ATail], [B | BTail]) ->
     (A * B) + vectorMultiplication(ATail, BTail).
     
+    
+%%%%%%%%%%%%%%%%%%%
+%% PROFILE STUFF %%
+%%%%%%%%%%%%%%%%%%%
+
+profile(A, B, P) ->
+    timer:tc(parallelMatrixMultiplication, matrixMultiplication, [A, B, P]).
+
+test_matrixMultiplication(A, B, P, PerfFile) ->
+    {Time, Result} = profile(A, B, P),
+    file:write(PerfFile, io_lib:fwrite("~w\n", [Time])), Result.
+
+test_looper(_, _, _, 0, _) ->
+    ok;
+test_looper(A, B, P, Index, PerfFile) ->
+    test_matrixMultiplication(A, B, P, PerfFile),
+    test_looper(A, B, P, Index-1, PerfFile).
+
+granted_int(Num) when erlang:is_integer(Num) -> 
+    Num;
+granted_int(Num) when erlang:is_list(Num) -> 
+    {NewNum, _} = string:to_integer(Num),
+    NewNum;
+granted_int(Num) when erlang:is_atom(Num) -> 
+    granted_int(erlang:atom_to_list(Num)).
+
+problemFromFileAux(_, eof) ->
+    [];
+problemFromFileAux(Device, Line) ->
+    [[granted_int(Num) || Num <- string:tokens(Line, " ")]] ++ problemFromFileAux(Device, io:get_line(Device, "")).
+
+problemFromFileAux(Device) ->
+    problemFromFileAux(Device, io:get_line(Device, "")).
+
+matrixFromFile(Filename) ->
+    {ok, File} = file:open(Filename, [read]),
+    Matrix = problemFromFileAux(File),
+    file:close(File),
+    Matrix.
+
+test_loop(AFile, BFile, P, Repetitions, PerfFile) ->
+    {ok, PFile} = file:open(PerfFile, [write]),
+    Result = test_looper(matrixFromFile(AFile), matrixFromFile(BFile), granted_int(P), granted_int(Repetitions), PFile),
+    file:close(PFile),
+    Result.
+
+test_loop([AFile, BFile, P, Repetitions, PerfFile]) ->
+    test_loop(AFile, BFile, P, Repetitions, PerfFile).
+
