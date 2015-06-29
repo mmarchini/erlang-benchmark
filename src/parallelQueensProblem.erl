b0d452c441a3f2885a5f07d1cc3d260b0517efcc
Matheus Marchini <matheusdot@gmail.com>
2015-06-29 19:13:14 -0300

    Refinando o código

--- src/parallelQueensProblem.erl
+++ src/parallelQueensProblem.erl
@@ -1,6 +1,6 @@
 -module(parallelQueensProblem).
 -import(lists, [seq/2]).
--export([queensResolver/2, queensResolverAux/4, queenResolverSpawn/5, test_loop/1, test_loop/4]).
+-export([queensResolver/2, queensResolverAux/4, queenResolverSpawn/5, test_loop/1, test_loop/4, profile/2]).
 
 % Tabuleiro NxN
 % N² possibilidades de posicionamento inicial (primeira peça)
@@ -8,9 +8,9 @@
 % Quantidade máxima de processos: N
 % Restrição de processos: N/P deve ser um número interio
 
-gather([H|T]) ->
+gather([_|T]) ->
     receive
-        {H, Ret} -> lists:umerge(Ret, gather(T))
+        Ret -> Ret ++ gather(T)
     end;
 gather([]) ->
     [].
@@ -22,27 +22,27 @@ queensResolver(N, P) when N rem P /= 0 ->
 queensResolver(N, P) ->
     Mult = (N div P),
     S = self(),
-    gather([spawn(parallelQueensProblem, queenResolverSpawn, [S, N, (Index+1)*Mult, 0, Index*Mult]) || Index <- seq(0, P-1)]).
+    gather([spawn(parallelQueensProblem, queenResolverSpawn, [S, N, (Index+1)*Mult, Index*Mult, 0]) || Index <- seq(0, P-1)]).
 
-queenResolverSpawn(S, N, ColEnd, Row, Column) ->
-    S ! {self(), queensResolverAux(N, ColEnd, Row, Column)}.
+queenResolverSpawn(S, N, RowEnd, Row, Column) ->
+    S ! queensResolverAux(N, RowEnd, Row, Column).
 
-queensResolverAux(_, ColEnd, _, Column) when (Column == ColEnd) ->
+queensResolverAux(N, _, _, Column) when (Column == N) ->
     [];
-queensResolverAux(N, ColEnd, Row, Column) when Row == N ->
-    queensResolverAux(N, ColEnd, 0, Column+1);
+queensResolverAux(_, RowEnd, Row, _) when Row == RowEnd ->
+    []; % queensResolverAux(N, ColEnd, 0, Column+1);
 queensResolverAux(N, ColEnd, Row, Column) ->
-    lists:umerge(evaluateSolution(N, Row, Column), queensResolverAux(N, ColEnd, Row+1, Column)).
+    evaluateSolution(N, Row, Column) ++ queensResolverAux(N, ColEnd, Row+1, Column).
 
 evaluateSolution(N, Row, Column) -> 
     evaluator(seq(0, N-1) -- [Row], seq(0, N-1) -- [Column], [{Row, Column}]).
 
 evaluator([], [], Pieces) ->
-    [lists:keysort(2, Pieces)];
+    [Pieces];
 evaluator(Rows, Columns, _) when length(Rows) /= length(Columns) ->
     [];
 evaluator(Rows, [Column | Columns], Pieces) ->
-    lists:umerge([evaluator(Rows -- [Row], Columns, Pieces ++ [{Row, Column}]) || Row <- Rows, lists:all(fun ({PRow, PCol}) -> abs(PRow - Row) /= abs(PCol - Column) end, Pieces)]).
+    lists:merge([evaluator(Rows -- [Row], Columns, Pieces ++ [{Row, Column}]) || Row <- Rows, lists:all(fun ({PRow, PCol}) -> abs(PRow - Row) /= abs(PCol - Column) end, Pieces)]).
 
 %%%%%%%%%%%%%%%%%%%
 %% PROFILE STUFF %%
--- src/queensProblem.erl
+++ src/queensProblem.erl
@@ -1,6 +1,6 @@
 -module(queensProblem).
 -import(lists, [seq/2]).
--export([queensResolver/1, test_loop/1, test_loop/3]).
+-export([queensResolver/1, profile/1, test_loop/1, test_loop/3]).
 
 % Tabuleiro NxN
 % N² possibilidades de posicionamento inicial (primeira peça)
@@ -13,19 +13,17 @@ queensResolver(N) ->
 queensResolverAux(N, _, Column) when (Column == N) ->
     [];
 queensResolverAux(N, Row, Column) when Row == N ->
-    queensResolverAux(N, 0, Column+1);
+    [];%queensResolverAux(N, 0, Column+1);
 queensResolverAux(N, Row, Column) ->
-    lists:umerge(evaluateSolution(N, Row, Column), queensResolverAux(N, Row+1, Column)).
+    evaluateSolution(N, Row, Column) ++ queensResolverAux(N, Row+1, Column).
 
 evaluateSolution(N, Row, Column) -> 
     evaluator(seq(0, N-1) -- [Row], seq(0, N-1) -- [Column], [{Row, Column}]).
 
 evaluator([], [], Pieces) ->
-    [lists:keysort(2, Pieces)];
-evaluator(Rows, Columns, _) when length(Rows) /= length(Columns) ->
-    [];
-evaluator(Rows, [Column | Columns], Pieces) ->
-    lists:umerge([evaluator(Rows -- [Row], Columns, Pieces ++ [{Row, Column}]) || Row <- Rows, lists:all(fun ({PRow, PCol}) -> abs(PRow - Row) /= abs(PCol - Column) end, Pieces)]).
+    [Pieces];
+evaluator(Rows, [Column | Columns], Pieces) when length(Rows) == length(Columns)+1 ->
+    lists:merge([evaluator(Rows -- [Row], Columns, Pieces ++ [{Row, Column}]) || Row <- Rows, lists:all(fun ({PRow, PCol}) -> abs(PRow - Row) /= abs(PCol - Column) end, Pieces)]).
 
 %%%%%%%%%%%%%%%%%%%
 %% PROFILE STUFF %%
